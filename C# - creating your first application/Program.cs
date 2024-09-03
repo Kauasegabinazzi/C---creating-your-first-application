9704819097dfabe14b35ff038be18f4588f27d04
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-02 22:20:29 -0300

    3-commit

--- C# - creating your first application/Program.cs
+++ C# - creating your first application/Program.cs
@@ -1,10 +1,19 @@
-﻿string message = "test";
-Console.WriteLine(message);
+﻿
+showMessage();
 
-string course = "C#: creating your first application";
-Console.WriteLine(course);
 
-string name = "Arthur";
-string lastName = "Fernandes";
+void showMessage()
+{
+    string message = "Wellcome";
+    Console.WriteLine("********************");
+    Console.WriteLine(message);
+    Console.WriteLine("********************");
+}
 
-Console.WriteLine(name + lastName);
+//string course = "C#: creating your first application";
+//Console.WriteLine(course);
+
+//string name = "Arthur";
+//string lastName = "Fernandes";
+
+//Console.WriteLine(name + lastName);
