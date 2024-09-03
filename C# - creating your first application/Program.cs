6821c7a54a0694e61eacf4f2513390c7e71b79f2
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-02 22:28:12 -0300

    4-commit

--- C# - creating your first application/Program.cs
+++ C# - creating your first application/Program.cs
@@ -1,14 +1,31 @@
 ﻿
-showMessage();
-
-
 void showMessage()
 {
     string message = "Wellcome";
-    Console.WriteLine("********************");
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
     Console.WriteLine(message);
-    Console.WriteLine("********************");
 }
+
+void showOptions()
+{
+    Console.WriteLine("\ntype 1 to register a new band");
+    Console.WriteLine("type 2 to show all bands");
+    Console.WriteLine("type 3 to review a band");
+    Console.WriteLine("type 4 to show average of indicators");
+    Console.WriteLine("type -1 to exit");
+}
+
+
+showMessage();
+showOptions();
 
 //string course = "C#: creating your first application";
 //Console.WriteLine(course);
